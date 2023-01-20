4adf03d5a2bf7d68fcb356b0d9260b34515850b8
Felix Anducho <felix.anducho74@gmail.com>
2023-01-19 18:26:06 -0600

    Add logic and styles to show feedback on required errors

--- src/components/atoms/FieldInput.tsx
+++ src/components/atoms/FieldInput.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 type FieldInputProps = {
   label: string;
   labelFor: string;
@@ -16,21 +18,51 @@ const FieldInput = ({
   labelColor,
   inputColor,
 }: FieldInputProps) => {
+  const [requiredError, setRequiredError] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+
+  function handleOnBlur() {
+    if (inputValue === "") {
+      return setRequiredError(true);
+    }
+    setRequiredError(false);
+  }
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setInputValue(event.target.value);
+  }
+
   return (
     <div className='input-container'>
-      <label
-        className='input-label'
-        htmlFor={labelFor}
-        style={{
-          color: labelColor != undefined ? labelColor : "",
-        }}>
-        {label}
-      </label>
+      <div className='label-container'>
+        <label
+          className='input-label'
+          htmlFor={labelFor}
+          style={{
+            color: labelColor != undefined ? labelColor : "",
+          }}>
+          {label}
+        </label>
+
+        {required && requiredError ? (
+          <label className='input-label red-label'>
+            This field is required
+          </label>
+        ) : (
+          ""
+        )}
+      </div>
+
       <input
         className='input-field'
         required={required}
         placeholder={placeholder}
+        onChange={handleInputChange}
+        value={inputValue}
+        onFocus={() => setRequiredError(false)}
+        onBlur={handleOnBlur}
         style={{
+          borderColor: required && requiredError ? "red" : "",
           color: inputColor != undefined ? inputColor : "",
         }}></input>
     </div>
