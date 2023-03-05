deb7e07bfaebd81604e0f0fceaf38bc4d18ee3df
Felix Anducho <felix.anducho74@gmail.com>
2023-03-05 00:02:33 -0600

    Finished basic structure of component

--- src/components/molecules/HorizontalCard/HorizontalCard.tsx
+++ src/components/molecules/HorizontalCard/HorizontalCard.tsx
@@ -1,7 +1,40 @@
 import styles from "horizontalCard.module.css";
+import CustomCheckBox from "../../atoms/CustomCheckBox/CustomCheckBox";
 
-const HorizontalCard = () => {
-  return <div></div>;
+type AddOn = {
+  title: string;
+  description: string;
+};
+
+type HorizontalCardProps = {
+  addOnName: string;
+  price: string | number;
+  details: AddOn;
+  timePrice: string;
+};
+
+const HorizontalCard = ({
+  addOnName,
+  price,
+  details,
+  timePrice,
+}: HorizontalCardProps) => {
+  return (
+    <article className={styles.horizotalCard_container}>
+      <div className={styles.horizotalCard_checkbox}>
+        <CustomCheckBox name={addOnName}></CustomCheckBox>
+      </div>
+      <div className={styles.horizotalCard_details}>
+        <h3>{details.title}</h3>
+        <p>{details.description}</p>
+      </div>
+      <div className={styles.horizontalCard_price}>
+        <p>
+          +${price}/{timePrice}{" "}
+        </p>
+      </div>
+    </article>
+  );
 };
 
 export default HorizontalCard;
