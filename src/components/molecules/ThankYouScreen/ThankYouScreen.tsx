bf4a92af7401a00f663298c3ed2662c94c7cf2b8
Felix Anducho <felix.anducho74@gmail.com>
2023-03-20 17:40:52 -0600

    Add styles to thank you component

--- src/components/molecules/ThankYouScreen/ThankYouScreen.tsx
+++ src/components/molecules/ThankYouScreen/ThankYouScreen.tsx
@@ -1,11 +1,12 @@
 import thankYouIcon from "../../../../assets/images/icon-thank-you.svg";
+import styles from "./thankYouScreen.module.css";
 
 const ThankYouScreen = () => {
   return (
-    <div>
+    <div className={styles.thankYouContainer}>
       <img src={thankYouIcon} alt='Confirm image' />
-      <h1>Thank you!</h1>
-      <p>
+      <h1 className={styles.thankYouTitle}>Thank you!</h1>
+      <p className={styles.thankYouPhrase}>
         Thanks for confirming your subscrition! We hope you have fun using our
         platform. If you ever need support, please feel free to email us at
         support@loremgamimg.com
